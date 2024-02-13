47d3e57b3370610ce20504c00e0a4ddc7faf2f4c
Riicarus <riicarus.acc@gmail.com>
2024-02-14 01:08:31 +0800

    update: dmenu -> rofi

--- config.h
+++ config.h
@@ -62,7 +62,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_lime, "-sf", col_gray4, NULL };
+/* Require: rofi */
+static const char *dmenucmd[] = { "rofi", "-combi-modi", "drun,ssh", "-font", "hack 10", "-show", "combi", "-theme", "~/.config/rofi/config.rasi", "-show-icons", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
