1de87056460685ea3307306d5f044e24e8100332
Riicarus <2895054643@qq.com>
2024-03-02 17:55:28 +0800

    update bar color

--- config.h
+++ config.h
@@ -15,15 +15,15 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-// static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#005577";
 static const char col_lime[]        = "#2b333e";
 static const char col_moon_white[]  = "#eef7f2";
 static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha  = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray1, col_gray3 },
-	[SchemeSel]  = { col_gray4, col_gray1, col_moon_white },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_gray4, col_cyan, col_moon_white },
 };
 
 static const unsigned int alphas[][3]      = {
